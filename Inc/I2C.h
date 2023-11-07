9cfd02ad0f4e16e1ff34aeeca9c4def1711e8f52
HairuMossa <hayrufetu@gmail.com>
2023-11-07 19:38:50 +0300

    Exclude Debug Folder from Track

--- Inc/I2C.h
+++ Inc/I2C.h
@@ -10,7 +10,7 @@
 
 #ifndef I2C_H_
 #define I2C_H_
-
+fd
 void I2C1_init(void);
 void I2C1_byteRead(char saddr, char maddr, char* data);
 void I2C1_burstRead(char saddr , char maddr, int n, char* data);
